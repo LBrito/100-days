da0f5cea8da82788f85974d74a03afb86ba8844d
L. Brito <ing.l.brito@gmail.com>
2019-01-25 01:04:33 -0800

    D&D

---
+++ python/dnd-character/dnd_character.py
@@ -0,0 +1,19 @@
+import random
+
+
+def modifier(value):
+    return (value - 10) // 2
+
+
+class Character:
+    def __init__(self):
+        self.strength = self.ability()
+        self.dexterity = self.ability()
+        self.constitution = self.ability()
+        self.intelligence = self.ability()
+        self.wisdom = self.ability()
+        self.charisma = self.ability()
+        self.hitpoints = 10 + modifier(self.constitution)
+
+    def ability(self):
+        return sum(sorted([random.randint(1, 6) for i in range(4)])[1:])
