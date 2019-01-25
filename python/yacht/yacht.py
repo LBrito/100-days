9ef5ff31ed87160ac15f9ff3e8e1ac51e0d9f05b
Brito Mendoza, Luis Alberto <luis.brito@thermofisher.com>
2019-01-24 18:46:22 -0800

    Yatch 2

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,18 +1,22 @@
+from collections import Counter
 # Score categories
 # Change the values as you see fit
-YACHT = None
-ONES = None
-TWOS = None
-THREES = None
-FOURS = None
-FIVES = None
-SIXES = None
-FULL_HOUSE = None
-FOUR_OF_A_KIND = None
-LITTLE_STRAIGHT = None
-BIG_STRAIGHT = None
-CHOICE = None
+YACHT = (lambda dice: 50 if dice.count(dice[0]) == len(dice) else 0)
+ONES = (lambda dice: add_each(dice, 1))
+TWOS = (lambda dice: add_each(dice, 2))
+THREES = (lambda dice: add_each(dice, 3))
+FOURS = (lambda dice: add_each(dice, 4))
+FIVES = (lambda dice: add_each(dice, 5))
+SIXES = (lambda dice: add_each(dice, 6))
+FULL_HOUSE = (lambda dice: sum(dice) if sorted(Counter(dice).values()) == [2, 3] else 0)
+FOUR_OF_A_KIND = (lambda dice: sum(set(map(lambda number: number if dice.count(number) >= 4 else 0, dice))) * 4)
+LITTLE_STRAIGHT = (lambda dice: 30 if list(range(1, 6)) == sorted(dice) else 0)
+BIG_STRAIGHT = (lambda dice: 30 if list(range(2, 7)) == sorted(dice) else 0)
+CHOICE = sum
+
+
+def add_each(dice, number): return dice.count(number) * number
 
 
 def score(dice, category):
-    pass
+    return category(dice)
